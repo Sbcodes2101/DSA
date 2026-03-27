e1797f5fb7448f07b83a1be96b5c416947c9d881
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-03-27 10:11:29 +0530

    Time: 2 ms (73.28%) | Memory: 10.1 MB (75.59%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<string> &board,vector<vector<string>> &ans){
-        int n = board.size();
+    bool isSafe(int row, int col, vector<string> &board){
+        int n=board.size();
         int duprow = row;
         int dupcol = col;
-        while(row>=0 && col>=0){
+
+        while(col>=0){
             if(board[row][col]=='Q') return false;
             col--;
         }
 
-        row = duprow;
-        col = dupcol;
+         row = duprow;
+         col = dupcol;
         while(row>=0 && col>=0){
             if(board[row][col]=='Q') return false;
-            row--;
-            col--;
+            row--;col--;
         }
 
-        row = duprow;
-        col = dupcol;
+         row = duprow;
+         col = dupcol;
         while(row<n && col>=0){
             if(board[row][col]=='Q') return false;
-            col--;
-            row++;
+            row++;col--;
         }
-
         return true;
     }
 
-    void solve(int col, vector<vector<string>> &ans, vector<string> &board, int n){
+    void solve(int col, vector<string> &board, vector<vector<string>> &ans, int n){
         if(col==n){
             ans.push_back(board);
             return;
         }
 
         for(int i=0;i<n;i++){
-            if(isSafe(i,col,board,ans)){
+            if(isSafe(i,col,board)){
                 board[i][col] = 'Q';
-                solve(col+1,ans,board,n);
-                board[i][col] = '.';
+                solve(col+1,board,ans,n);
+                board[i][col]='.';
             }
         }
-    
     }
 
     vector<vector<string>> solveNQueens(int n) {
@@ -51,7 +48,8 @@ public:
         for(int i=0;i<n;i++){
             board[i] = s;
         }
-        solve(0, ans, board, n);
+
+        solve(0,board,ans,n);
         return ans;
     }
 };
