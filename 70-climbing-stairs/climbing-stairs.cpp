7f581dcf22391bb31408919aee78cea5a1773b20
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-03-31 22:59:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.11%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int ans(vector<int> &dp,int n){
+        if(n==0) return 1;
+        if(n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        int left = ans(dp,n-1);
+        int right = ans(dp,n-2);
+        return dp[n] = left+right;
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return ans(dp,n);
+    }
+};
