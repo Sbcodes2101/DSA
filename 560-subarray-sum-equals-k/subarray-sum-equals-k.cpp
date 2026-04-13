0ef47c96ca8e2467a58ba61e7afed91330086140
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-04-13 09:23:21 +0530

    Time: 2477 ms (14.04%) | Memory: 34.2 MB (99.28%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,60 +1,17 @@
 class Solution {
 public:
-    // it is subarray and not subsequence
-    // int subsum(vector<int>& nums,int k,int i,int count){
-    //     int n=nums.size();
-    //     if(i>=n-1) return count;
-    //     if(nums[i]==k) {
-    //         count++;
-    //     }
-
-    //     int select = subsum(nums,k-nums[i],i+1,count);
-    //     int notselect = subsum(nums,k,i+1,count);
-
-    //     return select+notselect;
-    // }
-
     int subarraySum(vector<int>& nums, int k) {
-        // brute force using exra space maps
-        // int n=nums.size();
-        // unordered_map<int,int> mp;
-        // int count=0;
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]==k) count++;
-        //     if(k>nums[i]){
-        //         if(mp.find(k-nums[i])==mp.end()){
-        //             mp[nums[i]]=i;
-        //         }
-        //         else if(mp.find(k-nums[i])!=mp.end()) count++;
-        //     }
-        // }
-        // return count;
-        // using recursion
-            // int count=0;
-            // return subsum(nums,k,0,count);
-            // return count;
+        int n = nums.size();
+        int ans = 0;
 
-        // using prefix sum(most oprimized)
-        int n=nums.size();
-        int count=0;
-        vector<int> prefix(n);
-
-        prefix[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+nums[i];
-        }
-        
-        unordered_map<int,int> mp;
-
-        for(int j=0;j<n;j++){
-            if(prefix[j]==k) count++;
-            int val=prefix[j]-k;
-
-            if(mp.find(val)!=mp.end()){
-                count += mp[val];
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                sum += nums[j];
+                if(sum == k) ans++;
             }
-            mp[prefix[j]]++;
         }
-        return count;
+
+        return ans;
     }
 };
