c7029f20762c3ecbfc2385b1e9cee6255c072046
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-04-13 10:07:44 +0530

    Time: 55 ms (38.06%) | Memory: 49.1 MB (20.25%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-       map<int,int> mp;
-       mp[0] = 1;
-       int presum=0;
-       int cnt=0;
-       for(int i=0;i<nums.size();i++){
-        presum += nums[i];
-        int remove = presum-k;
-        if(mp.find(remove)!=mp.end()){
-            cnt += mp[remove];
+        int n=nums.size();
+        map<int,int> mp;
+        vector<int> prefix(n);
+        int cnt=0;
+
+        prefix[0] = nums[0];
+
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i-1]+nums[i];
         }
-        mp[presum]++;
-       }
-       return cnt;
+
+        for(int i=0;i<n;i++){
+            if(prefix[i]==k) cnt++;
+            int remove = prefix[i]-k;
+            if(mp.find(remove)!=mp.end()){
+                cnt += mp[remove];
+            }   
+            mp[prefix[i]]++;         
+        }
+        return cnt;
     }
 };
