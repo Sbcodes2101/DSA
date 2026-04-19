71352d9b13601c7114f8614adad22fc1b4c4b6ab
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-04-19 18:09:25 +0530

    Time: 11 ms (43.97%) | Memory: 11 MB (80.89%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -19,13 +19,12 @@ public:
         int count = 0;
         int lastseen[3] = {-1,-1,-1};
         int n = s.size();
-        for(int i=0;i<n;i++){
+        for(int i=n-1;i>=0;i--){
             lastseen[s[i]-'a'] = i;
             if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1){
-                count += 1+min(lastseen[0],min(lastseen[1],lastseen[2]));
+                count += n-max(lastseen[0],max(lastseen[1],lastseen[2]));
             }
         }
-
         return count;
     }
 };
