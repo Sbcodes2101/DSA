9641194a30920ae82e442eef9f9853f1ae3ae0da
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-04-21 09:15:06 +0530

    Time: 31 ms (52.78%) | Memory: 44.3 MB (22.75%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -12,10 +12,23 @@ public:
         return dp[i][j]=solve(s,t,i-1,j,dp);
     }
 
-    int numDistinct(string s, string t) {
+    int numDistinct(string s, string t){
         int n=s.size();
         int m=t.size();
-        vector<vector<long long>> dp(n,vector<long long> (m,-1));
-        return (int)solve(s,t,n-1,m-1,dp);
+        vector<vector<double>> dp(n+1,vector<double> (m+1,0));
+        // return (int)solve(s,t,n-1,m-1,dp);
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]) dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return (int)dp[n][m];
     }
 };
