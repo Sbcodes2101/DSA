1e1dc5a295af9808b75b8e3db0c3c53a5c139bed
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-05-07 09:13:21 +0530

    Time: 1859 ms (5.01%) | Memory: 429.5 MB (7.02%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool check(int i,int j,string s,vector<vector<int>> &dp){
+        if(i>j){
+           return true; 
+        }
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(s[i]==s[j]){
+            return dp[i][j] = check(i+1,j-1,s,dp);
+        }
+
+        return dp[i][j]=false;
+    }
+
+    int countSubstrings(string s) {
+        int n = s.size();
+        int count = 0;
+        vector<vector<int>> dp(n,vector<int> (n,-1));
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(check(i,j,s,dp)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
