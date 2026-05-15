0237690580dc0f09d414971eac366ce45ad6c849
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-05-15 10:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (86.69%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    // int f(string &s,int idx,vector<int> &dp){
-    //     if(idx==s.size()){
-    //         return 1;
-    //     }
-    //     if(s[idx]=='0') return 0;
-    //     if(dp[idx]!=-1) return dp[idx];
+    int f(string &s,int idx,vector<int> &dp){
+        if(idx==s.size()){
+            return 1;
+        }
 
-    //     int only_one_char = f(s,idx+1,dp);
-    //     int only_two_char = 0;
+        if(dp[idx]!=-1) return dp[idx];
 
-    //     if(idx+1<s.size()) {if(s[idx]=='1' || (s[idx]=='2' && s[idx+1]<='6'))
-    //     {only_two_char = f(s,idx+2,dp);}}
+        if(s[idx]=='0') return 0;
 
-    //     return dp[idx] = only_one_char+only_two_char;
-    // }
+        int pick_one_char = f(s,idx+1,dp);
+        int pick_two_char = 0;
+
+        if(idx+1<s.size()){
+            if(s[idx]=='1' || s[idx]=='2' && s[idx+1]<='6'){
+                pick_two_char = f(s,idx+2,dp);
+            }
+        }
+
+        return dp[idx] = pick_one_char+pick_two_char;
+    }
 
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n+1,0);
-        // return f(s,0,dp);
-        dp[n] = 1;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='0') dp[i] = 0;
-            else{
-                dp[i] += dp[i+1];
-                if(i+1<n){
-                    if(s[i]=='1' || s[i]=='2' && s[i+1]<='6'){
-                        dp[i] += dp[i+2];
-                    }
-                }
-            }
-        }
-        return dp[0];
+        vector<int> dp(n,-1);
+        return f(s,0,dp);
     }
 };
