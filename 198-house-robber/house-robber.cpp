297bee13a2f990813f38b84b1986cbebd4af9a1f
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-05-15 11:49:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (20.40%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,48 +1,20 @@
 class Solution {
 public:
-    // Memoization
-    // int f(int idx,vector<int> &nums,vector<int> &dp){
-    //     if(idx==0) return nums[idx];
-    //     if(idx<0) return 0;
-    //     if(dp[idx]!=-1) return dp[idx];
+    int f(vector<int> &nums,vector<int> &dp,int idx){
+        if(idx==0) return nums[0];
+        if(idx<0) return 0;
 
-    //     int pick = nums[idx]+f(idx-2,nums,dp);
-    //     int notpick = f(idx-1,nums,dp);
+        if(dp[idx]!=-1) return dp[idx];
 
-    //     return dp[idx] = max(pick,notpick);
-    // }
+        int pick = nums[idx] + f(nums,dp,idx-2);
+        int not_pick = f(nums,dp,idx-1);
+
+        return dp[idx] = max(pick,not_pick);
+    }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        // vector<int> dp(n,-1);
-        // return f(n-1,nums,dp); memoization
-        // tabulation
-        // dp[0] = nums[0];
-        // for(int i=1;i<n;i++){
-        //     int pick = nums[i];
-        //     if(i>1) pick += dp[i-2];
-        //     int notpick = dp[i-1];
-
-        //     dp[i] = max(pick,notpick);
-        // }
-
-        // return dp[n-1];
-
-        // space optimization
-        int prev = nums[0];
-        int prev2 = 0;
-        if(nums.size()==1) return nums[0];
-        int curr;
-        for(int i=1;i<n;i++){
-        int pick = nums[i];
-        if(i>1) pick += prev2;
-        int notpick = prev;
-        curr = max(pick,notpick);
-
-        prev2 = prev;
-        prev = curr;
+        vector<int> dp(n,-1);
+        return f(nums,dp,n-1);
     }
-     return curr;
-    }
-   
 };
