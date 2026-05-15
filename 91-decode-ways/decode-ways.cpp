6de1186348f60236e0e0587b1dd7a8f4e7cd704a
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-05-15 10:57:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (50.42%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -23,7 +23,22 @@ public:
 
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n,-1);
-        return f(s,0,dp);
+        vector<int> dp(n+1,0);
+        // return f(s,0,dp);
+
+        dp[n] = 1;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0') dp[i]=0;
+            else{
+                dp[i] += dp[i+1];
+                if(i+1<s.size()){
+                    if(s[i]=='1' || s[i]=='2' && s[i+1]<='6'){
+                        dp[i] += dp[i+2];
+                    }
+                }
+            }
+        }
+
+        return dp[0];
     }
 };
