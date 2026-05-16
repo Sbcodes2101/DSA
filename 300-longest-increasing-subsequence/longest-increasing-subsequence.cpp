d9d69c1904d347c6f34645a1e84e2559a31de884
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-05-16 11:28:05 +0530

    Time: 559 ms (13.89%) | Memory: 296.5 MB (10.74%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    int f(int idx,int prev_idx,vector<int> &nums,vector<vector<int>> &dp){
+    int f(vector<int> &nums,int idx,int prev_idx,vector<vector<int>> &dp){
         if(idx==nums.size()){
             return 0;
         }
-        if(dp[idx][prev_idx+1] != -1) return dp[idx][prev_idx+1];
+
+        if(dp[idx][prev_idx+1]!=-1) return dp[idx][prev_idx+1];
+
+        int not_pick = f(nums,idx+1,prev_idx,dp);
         int pick = INT_MIN;
-        int notpick = 0+f(idx+1,prev_idx,nums,dp);
-        if(prev_idx == -1 || nums[idx]>nums[prev_idx]){
-            pick = 1+f(idx+1,idx,nums,dp);
+        if(prev_idx == -1 || nums[prev_idx]<nums[idx]){
+            pick = 1+f(nums,idx+1,idx,dp);
         }
 
-        return dp[idx][prev_idx+1]=max(pick,notpick);
+        return dp[idx][prev_idx+1] = max(pick,not_pick);
     }
 
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,1);
-        int maxlen=1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    dp[i] = max(dp[i],dp[j]+1);
-                    maxlen = max(maxlen,dp[i]);
-                }
-            }
-        }
-
-        return maxlen;
+        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
+        return f(nums,0,-1,dp);
     }
 };
