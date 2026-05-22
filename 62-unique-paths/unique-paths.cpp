f8018a0003169cb91dc8a7944859c2a789011766
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-05-22 08:28:27 +0530

    Time: 3 ms (9.29%) | Memory: 9.4 MB (50.74%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -4,35 +4,18 @@ public:
         if(i==0 && j==0){
             return 1;
         }
+
         if(i<0 || j<0) return 0;
 
-        if(dp[i][j]!=-1) return dp[i][j];
+         if(dp[i][j]!=-1) return dp[i][j];
 
         int up = f(i-1,j,dp);
         int left = f(i,j-1,dp);
 
-        return dp[i][j] = up+left;
+        return dp[i][j]=up+left;
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int> (n,-1));
-        // return f(m-1,n-1,dp);
-        vector<int> prev(n,0);
-        for(int i=0;i<m;i++){
-            vector<int> curr(n,0);
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) {
-                    curr[j]=1;
-                }
-                else{
-                int up =0;int left = 0;
-                if(i>0) up =prev[j];
-                if(j>0) left =curr[j-1];
-                curr[j] = up+left;
-                }
-            }
-            prev = curr;
-        }
-
-        return prev[n-1];
+        return f(m-1,n-1,dp);
     }
 };
