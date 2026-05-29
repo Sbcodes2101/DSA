ed30e3575d957a00dd03386bae525ad62aa93f93
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-05-29 17:56:33 +0530

    Time: 60 ms (6.39%) | Memory: 85 MB (34.65%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        // Brute force approach
-        // int maxlen=INT_MIN;
-        // for(int i=0;i<fruits.size();i++){
-        // set<int> st;
-        //     for(int j=i;j<fruits.size();j++){
-        //         st.insert(fruits[j]);
-        //         if(st.size()<=2) maxlen=max(maxlen,j-i+1);
-        //     }
-        // }
-        // return maxlen;
-        // sliding window
-        int n=fruits.size();
-        unordered_map<int,int> mp;
-        int maxlen = INT_MIN;
+        int n = fruits.size();
         int l=0;int r=0;
+        int maxlen = INT_MIN;
+        unordered_map<int,int> mp;
+
         while(r<n){
             mp[fruits[r]]++;
             if(mp.size()>2){
                 while(mp.size()>2){
                     mp[fruits[l]]--;
-                    if(mp[fruits[l]]==0) mp.erase(fruits[l]);
+                    if(mp[fruits[l]] == 0) mp.erase(fruits[l]);
                     l++;
                 }
             }
-            
-            maxlen=max(maxlen,r-l+1);
+
+            maxlen = max(maxlen,r-l+1);
             r++;
         }
 
