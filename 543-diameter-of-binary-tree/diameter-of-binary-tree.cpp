5ff1834acc85e32eb463c7bdd8c87f7f452a1865
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-05-29 06:52:01 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (17.70%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,29 +11,43 @@
  */
 class Solution {
 public:
-    int f(TreeNode* root){
+    // int f(TreeNode* root){
+    //     if(root==NULL) return 0;
+
+    //     int lft_hght = f(root->left);
+    //     int rght_hght = f(root->right);
+
+    //     return 1+max(lft_hght,rght_hght);
+    // }
+
+    int f(TreeNode* root,int &maxi){
         if(root==NULL) return 0;
 
-        int lft_hght = f(root->left);
-        int rght_hght = f(root->right);
+        int lft_hght = f(root->left,maxi);
+        int rght_hght = f(root->right,maxi);
+
+        maxi = max(maxi,lft_hght+rght_hght);
 
         return 1+max(lft_hght,rght_hght);
     }
 
     int diameterOfBinaryTree(TreeNode* root) {
+        // if(root==NULL) return 0;
+
+        // int lft_hght = f(root->left);
+        // int rght_hght = f(root->right);
+
+        // int curr_diameter = lft_hght + rght_hght;
+
+        // int lft = diameterOfBinaryTree(root->left);
+        // int rght = diameterOfBinaryTree(root->right);
+
+        // return max(curr_diameter,max(lft,rght));
+
+        // O(n) solution
         if(root==NULL) return 0;
-
-        int lft_hght = f(root->left);
-        int rght_hght = f(root->right);
-
-        int curr_diameter = lft_hght + rght_hght;
-
-        int lft = diameterOfBinaryTree(root->left);
-        int rght = diameterOfBinaryTree(root->right);
-
-        return max(curr_diameter,max(lft,rght));
-
-        
-
+        int maxi=INT_MIN;
+        f(root,maxi);
+        return maxi;
     }
 };
