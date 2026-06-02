ff2723e2e04eb6a4ed56fc75a1686da25bc32671
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-06-02 09:14:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -19,31 +19,31 @@ class Solution {
         // code here
         vector<int> ans;
         queue<pair<Node*,int>> q;
-        map<int,int> mp;
-        
         q.push({root,0});
+        map<int,int> mp;
         
         while(!q.empty()){
             auto it = q.front();
             q.pop();
             
             Node* node = it.first;
-            int line = it.second;
+            int level = it.second;
             
-            if(mp.find(line)==mp.end()) mp[line] = node->data;
+            if(mp.find(level)==mp.end()) mp[level] = node->data;
             
-            if(node->left!=NULL){
-                q.push({node->left,line-1});
+            if(node->left != NULL){
+                q.push({node->left,level-1});
             }
             
-            if(node->right!=NULL){
-                q.push({node->right,line+1});
+            if(node->right != NULL){
+                q.push({node->right,level+1});
             }
         }
         
         for(auto it:mp){
             ans.push_back(it.second);
         }
+        
         return ans;
     }
 };
