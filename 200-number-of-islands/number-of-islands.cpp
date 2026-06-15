fd17a4b431e10e86470ea4b0e92b7db4b0f88d76
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-06-15 08:54:51 +0530

    Time: 24 ms (77.69%) | Memory: 19.4 MB (35.55%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int rows[4] = {-1,1,0,0};
+    int col[4] = {0,0,-1,1};
+    int n;int m;
+    bool isVald(int i,int j){
+        return (i>=0 && j>=0 && i<n && j<m);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int ans=0;
+        n=grid.size();
+        m=grid[0].size();
+        vector<vector<int>> visited(n,vector<int> (m,0));
+        queue<pair<int,int>> q;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == '1'){
+                    ans++;
+                    q.push({i,j});
+                    grid[i][j] = '0';
+                    while(!q.empty()){
+                        int r = q.front().first;
+                        int c = q.front().second;
+                        q.pop();
+
+                        for(int i=0;i<4;i++){
+                            int nr = r + rows[i];
+                            int nc = c + col[i];
+
+                            if(isVald(nr,nc) && grid[nr][nc]=='1'){
+                                grid[nr][nc] = '0';
+                                q.push({nr,nc});
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
