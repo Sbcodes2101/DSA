4116193cdbcb02f38c795283ef6b2725fb950069
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-07-01 08:31:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (65.64%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int f(string &s,int idx,vector<int> &dp){
-        if(idx==s.size()){
-            return 1;
-        }
+        if(idx==s.size()) return 1;
 
         if(dp[idx]!=-1) return dp[idx];
 
         if(s[idx]=='0') return 0;
 
-        int pick_one_char = f(s,idx+1,dp);
+        int pick_one_char = f(s,idx+1,dp);     
         int pick_two_char = 0;
 
         if(idx+1<s.size()){
@@ -17,23 +15,24 @@ public:
                 pick_two_char = f(s,idx+2,dp);
             }
         }
-
-        return dp[idx] = pick_one_char+pick_two_char;
+        
+        return dp[idx]=pick_one_char+pick_two_char;
     }
+
 
     int numDecodings(string s) {
         int n = s.size();
         vector<int> dp(n+1,0);
         // return f(s,0,dp);
-
         dp[n] = 1;
+        
         for(int i=n-1;i>=0;i--){
             if(s[i]=='0') dp[i]=0;
             else{
                 dp[i] += dp[i+1];
                 if(i+1<s.size()){
                     if(s[i]=='1' || s[i]=='2' && s[i+1]<='6'){
-                        dp[i] += dp[i+2];
+                        dp[i]+=dp[i+2];
                     }
                 }
             }
