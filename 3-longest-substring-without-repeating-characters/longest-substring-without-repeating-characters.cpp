aec2248a339f2ce8abec40899bceaee2f04cdfd8
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-07-05 11:29:33 +0530

    Time: 15 ms (37.05%) | Memory: 14.4 MB (20.65%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        int maxi=0; 
+        // int n = s.size();
+        // int maxi=0; 
 
-        for(int i=0;i<n;i++){
-            unordered_set<char> st;
-            for(int j=i;j<n;j++){
-                char ch = s[j];
-                if(st.find(ch)!=st.end()){
-                    break;
+        // for(int i=0;i<n;i++){
+        //     unordered_set<char> st;
+        //     for(int j=i;j<n;j++){
+        //         char ch = s[j];
+        //         if(st.find(ch)!=st.end()){
+        //             break;
+        //         }
+
+        //         st.insert(ch);
+        //         maxi = max(j-i+1,maxi);
+        //     }
+        // }
+
+        // return maxi;
+
+        int n=s.size();
+        int ans = INT_MIN;
+        int i=0;
+        int j=0;
+        unordered_set<int> st;
+
+        while(j<n){
+            if(st.find(s[j])!=st.end()){
+                while(st.find(s[j])!=st.end()){
+                    st.erase(s[i]);
+                    i++;
                 }
-
-                st.insert(ch);
-                maxi = max(j-i+1,maxi);
             }
+
+            st.insert(s[j]);
+            ans = max(ans,j-i+1);
+            j++;
         }
 
-        return maxi;
+        return (ans==INT_MIN)? 0:ans;
     }
 };
