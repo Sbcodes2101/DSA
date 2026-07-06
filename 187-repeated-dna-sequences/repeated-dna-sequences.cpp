62e6db6f8e877bc167ffebb3061ea50817931e2c
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-07-06 09:39:24 +0530

    Time: 34 ms (88.05%) | Memory: 18.6 MB (90.51%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -2,16 +2,37 @@ class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
         vector<string> ans;
-        unordered_set<string> seen;
+        unordered_set<int> seen;
         unordered_set<string> result;
 
-        int n = s.size()-10;
-        for(int i=0;i<=n;i++){
-            string st = s.substr(i,10);
-            if(seen.find(st)!=seen.end()){
-                result.insert(st);
+        int sum=0;
+        int k = 10;
+        // "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+        //  i
+        unordered_map<char, int> mpp = {
+            {'A', 0},
+            {'C', 1},
+            {'G', 2},
+            {'T', 3}
+        };
+
+        for(int i=0;i<k;i++){
+            int power = k-i-1;
+            sum = sum + (pow(4,power)*mpp[s[i]]);
+        }
+
+        seen.insert(sum);
+
+        for(int i=k;i<s.size();i++){
+            sum = sum - (pow(4,k-1)*mpp[s[i-k]]);
+            sum = sum*4;
+            sum = sum+mpp[s[i]];
+
+            if(seen.find(sum)!=seen.end()){
+                result.insert(s.substr(i-k+1,k));
             }
-            seen.insert(st);
+
+            seen.insert(sum);
         }
 
         while(!result.empty()){
