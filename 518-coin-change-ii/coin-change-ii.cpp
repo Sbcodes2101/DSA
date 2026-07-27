6667a1987ea29bedb5cbf6871eb4eeb3b1ded82b
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-07-27 12:32:50 +0530

    Time: 31 ms (49.70%) | Memory: 37 MB (27.80%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -5,20 +5,23 @@ public:
             return 1;
         }
 
-        if(amount<0 || idx<0) return 0;
+        if(amount<0 || idx==0) return 0; 
 
         if(dp[idx][amount]!=-1) return dp[idx][amount];
 
         int pick = 0;
-        if(amount-coins[idx]>=0) pick = f(amount-coins[idx],coins,idx,dp);
+        if(amount-coins[idx-1]>=0) pick = f(amount-coins[idx-1],coins,idx,dp);
         int not_pick = f(amount,coins,idx-1,dp);
 
         return dp[idx][amount] = pick+not_pick;
-    }
+    } 
+
 
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int> (amount+1,-1));
-        return f(amount,coins,n-1,dp);
+        vector<vector<int>> dp(n+1,vector<int> (amount+1,-1));
+        return f(amount,coins,n,dp);
+
+
     }
 };
