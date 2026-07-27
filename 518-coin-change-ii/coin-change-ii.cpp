ad41e302f3db8866b57427ac06a344da6246ea5e
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-07-27 13:11:56 +0530

    Time: 43 ms (29.66%) | Memory: 61.6 MB (7.91%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -19,9 +19,20 @@ public:
 
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n+1,vector<int> (amount+1,-1));
-        return f(amount,coins,n,dp);
+        vector<vector<double>> dp(n+1,vector<double> (amount+1,0));
+        // return f(amount,coins,n,dp);
 
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
 
+        for(double i=1;i<=n;i++){
+            for(double j=1;j<=amount;j++){
+                if(j-coins[i-1]>=0) dp[i][j] = dp[i-1][j]+dp[i][j-coins[i-1]];
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+
+        return dp[n][amount];
     }
 };
