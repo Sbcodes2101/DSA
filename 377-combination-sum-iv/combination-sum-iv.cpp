2867b447b0f9de35d30b795cd9e6db21b9158553
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-07-28 12:23:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (12.86%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -19,7 +19,18 @@ public:
 
     int combinationSum4(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<int> dp(target+1,-1);
-        return findTotalSum(nums,target,n,dp);
+        vector<double> dp(target+1,0);
+        // return findTotalSum(nums,target,n,d);
+        dp[0]=1;
+
+        for(int i=1;i<=target;i++){
+            double result = 0;
+            for(int j=0;j<nums.size();j++){
+            if(i-nums[j]>=0) result += dp[i-nums[j]];
+            }
+            dp[i] = result;
+        }
+
+        return dp[target];
     }
 };
