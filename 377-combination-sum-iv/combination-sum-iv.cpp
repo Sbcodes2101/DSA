b6058f219c796701ff635c08c9e657178ed37575
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-07-28 11:51:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (20.57%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int findTotalSum(vector<int> &nums,int target,int idx,vector<vector<int>> &dp){
+    int findTotalSum(vector<int> &nums,int target,int idx,vector<int> &dp){
         if(target==0){
             return 1;
         }
 
-        if(target<0 || idx<0) return 0;
+        if(target<0) return 0;
 
-        if(dp[idx][target]!=-1) return dp[idx][target];
+        if(dp[target]!=-1) return dp[target];
 
         int ans=0;
         for(int i=0;i<nums.size();i++){
-            ans += findTotalSum(nums,target-nums[i],i,dp);
+            ans += findTotalSum(nums,target-nums[i],idx,dp);
         }
 
-        return dp[idx][target] = ans;
+        return dp[target] = ans;
     }
 
     int combinationSum4(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int> (target+1,-1));
+        vector<int> dp(target+1,-1);
         return findTotalSum(nums,target,n,dp);
     }
 };
