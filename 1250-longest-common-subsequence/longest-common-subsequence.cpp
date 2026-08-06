01617a66efd736f29dad44d63c4d76f696c12956
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-08-06 09:54:19 +0530

    Time: 27 ms (57.10%) | Memory: 27.5 MB (55.45%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    int f(string &text1, string &text2, int i, int j, vector<vector<int>> &dp){
-        if(i>=text1.size() || j>=text2.size()){
-            return 0;
-        }
+    int maxLength(string text1,string text2,int i,int j,vector<vector<int>> &dp){
+        if(i==0 || j==0) return 0;
 
         if(dp[i][j]!=-1) return dp[i][j];
 
-        if(text1[i]==text2[j]){
-           return dp[i][j] = 1 + f(text1,text2,i+1,j+1,dp);
+        if(text1[i-1]==text2[j-1]){
+            return dp[i][j] = 1+maxLength(text1,text2,i-1,j-1,dp);
         }
 
-        return dp[i][j] = max(f(text1,text2,i+1,j,dp),f(text1,text2,i,j+1,dp));
-
+        return dp[i][j] = max(maxLength(text1,text2,i-1,j,dp),maxLength(text1,text2,i,j-1,dp));
     }
 
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
         vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-        // return f(text1,text2,0,0,dp);
+        // return maxLength(text1,text2,n,m,dp);
 
-        for(int i=1;i<text1.size()+1;i++){
-            for(int j=1;j<text2.size()+1;j++){
-                if(text1[i-1]==text2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]) dp[i][j] = 1+dp[i-1][j-1];
                 else{
                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
+
         return dp[n][m];
     }
 };
