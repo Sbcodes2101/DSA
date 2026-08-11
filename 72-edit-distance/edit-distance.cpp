587e9243e592bf5c2b459657e7737875f952fce4
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-08-11 09:39:41 +0530

    Time: 1 ms (95.14%) | Memory: 12.4 MB (84.08%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -14,31 +14,57 @@ public:
         return dp[i][j] = min(insert,min(del,replace));
     }
     int minDistance(string word1, string word2) {
+        // int n = word1.size();
+        // int m = word2.size();
+        // vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        // // return f(word1,word2,n,m,dp);
+
+        // for(int j=0;j<=m;j++){
+        //     dp[0][j] = j;
+        // }
+
+        // for(int i=0;i<=n;i++){
+        //     dp[i][0] = i;
+        // }
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(word1[i-1]==word2[j-1]) dp[i][j] = dp[i-1][j-1];
+        //         else{
+        //             int insert = 1+dp[i][j-1];
+        //             int del = 1+dp[i-1][j];
+        //             int replace = 1+dp[i-1][j-1];
+        //             dp[i][j] = min(insert,min(del,replace));
+        //         }
+        //     }
+        // }
+
+        // return dp[n][m];
+
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-        // return f(word1,word2,n,m,dp);
 
-        for(int j=0;j<=m;j++){
-            dp[0][j] = j;
-        }
+        vector<int> prev(m+1,0);
 
-        for(int i=0;i<=n;i++){
-            dp[i][0] = i;
+        for(int i=0;i<=m;i++){
+            prev[i] = i;
         }
 
         for(int i=1;i<=n;i++){
+            vector<int> curr(m+1,0);
+            curr[0] = i;
             for(int j=1;j<=m;j++){
-                if(word1[i-1]==word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                if(word1[i-1]==word2[j-1]) curr[j] = prev[j-1];
                 else{
-                    int insert = 1+dp[i][j-1];
-                    int del = 1+dp[i-1][j];
-                    int replace = 1+dp[i-1][j-1];
-                    dp[i][j] = min(insert,min(del,replace));
+                    int insert = 1+curr[j-1];
+                    int del = 1+prev[j];
+                    int replace = 1+prev[j-1];
+                    curr[j] = min(insert,min(del,replace));
                 }
             }
+            prev=curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
