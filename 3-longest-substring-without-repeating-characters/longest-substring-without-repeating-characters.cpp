5b3ae70677448d4a3e5ec749f561e9dfd448cd32
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-08-12 21:43:16 +0530

    Time: 59 ms (16.70%) | Memory: 19.3 MB (15.77%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,64 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // int n = s.size();
-        // int maxi=0; 
-
-        // for(int i=0;i<n;i++){
-        //     unordered_set<char> st;
-        //     for(int j=i;j<n;j++){
-        //         char ch = s[j];
-        //         if(st.find(ch)!=st.end()){
-        //             break;
-        //         }
-
-        //         st.insert(ch);
-        //         maxi = max(j-i+1,maxi);
-        //     }
-        // }
-
-        // return maxi;
-
-        // int n=s.size();
-        // int ans = INT_MIN;
-        // int i=0;
-        // int j=0;
-        // unordered_set<int> st;
-
-        // while(j<n){
-        //     if(st.find(s[j])!=st.end()){
-        //         while(st.find(s[j])!=st.end()){
-        //             st.erase(s[i]);
-        //             i++;
-        //         }
-        //     }
-
-        //     st.insert(s[j]);
-        //     ans = max(ans,j-i+1);
-        //     j++;
-        // }
-
-        // return (ans==INT_MIN)? 0:ans;
-
-
-        int n=s.size();
-        int ans = INT_MIN;
-        int i=0;
-        int j=0;
-        unordered_map<char,int> st;
-
+        int n = s.size();
+        int i=0;int j=0;
+        int len = INT_MIN;
+        unordered_map<char,int> mp;
         while(j<n){
-            if(st.find(s[j])!=st.end()){
-                if(st[s[j]]>=i){
-                i = st[s[j]]+1;
-                }
+            if (mp.find(s[j]) != mp.end() && mp[s[j]] >= i) {
+                i = mp[s[j]] + 1;
             }
 
-            ans = max(ans,j-i+1);
-            st[s[j]] = j;
+            len = max(len,j-i+1);
+            mp[s[j]]=j;
             j++;
         }
 
-        return (ans==INT_MIN)? 0:ans;
+        return (len==INT_MIN)? 0:len;
     }
 };
