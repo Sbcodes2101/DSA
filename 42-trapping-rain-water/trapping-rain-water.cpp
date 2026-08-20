6d1199fee2793310b64f26de45b5b646bde11fb1
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-08-20 15:12:52 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (6.23%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int prefix[n];
-        prefix[0]=height[0];
+        vector<int> prevHeight(n);
+        prevHeight[0] = height[0];
+         
         for(int i=1;i<n;i++){
-            prefix[i] = max(prefix[i-1],height[i]);
+            prevHeight[i] = max(prevHeight[i-1],height[i]);
         }
-        int suffix[n];
-        suffix[n-1] = height[n-1]; 
+
+        vector<int> nxtHeight(n);
+        nxtHeight[n-1] = height[n-1];
         for(int i=n-2;i>=0;i--){
-            suffix[i] = max(suffix[i+1],height[i]);
+            nxtHeight[i] = max(nxtHeight[i+1],height[i]);
         }
-        int total = 0;
+
+        int ans = 0;
         for(int i=0;i<n;i++){
-            if(height[i]<prefix[i] && height[i]<suffix[i]){
-                total += min(prefix[i],suffix[i])-height[i];
+            if(height[i]<prevHeight[i] && height[i]<nxtHeight[i]){
+            ans += min(prevHeight[i],nxtHeight[i])-height[i];
             }
         }
-        return total;
+
+        return ans;
     }
 };
