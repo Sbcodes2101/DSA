56cda85761faf39f8d4c7919fc25cb114c50441e
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-08-20 14:20:50 +0530

    Time: 309 ms (10.35%) | Memory: 81.4 MB (11.70%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,18 +3,22 @@ public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
         int i=0;int j=0;
-        int len = INT_MIN;
-        unordered_map<char,int> mp;
+        int maxLen = 0;
+        unordered_map<int,int> mp;
+
         while(j<n){
-            if (mp.find(s[j]) != mp.end() && mp[s[j]] >= i) {
-                i = mp[s[j]] + 1;
+            if(mp.find(s[j])!=mp.end()){
+                while(mp.find(s[j])!=mp.end()){
+                mp.erase(s[i]);
+                i++;
+                }
             }
 
-            len = max(len,j-i+1);
-            mp[s[j]]=j;
+            maxLen = max(maxLen,j-i+1);
+            mp[s[j]] = j;
             j++;
         }
 
-        return (len==INT_MIN)? 0:len;
+        return maxLen;
     }
 };
