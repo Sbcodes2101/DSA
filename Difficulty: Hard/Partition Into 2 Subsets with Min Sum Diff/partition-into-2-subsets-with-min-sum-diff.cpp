f184db49e2db68e594979a4fdd8afbc833a9807a
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-08-30 10:22:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Partition Into 2 Subsets with Min Sum Diff/partition-into-2-subsets-with-min-sum-diff.cpp
+++ Difficulty: Hard/Partition Into 2 Subsets with Min Sum Diff/partition-into-2-subsets-with-min-sum-diff.cpp
@@ -1,29 +1,29 @@
 class Solution {
   public:
-    int f(vector<int>& arr,int idx,int sum1,int target,vector<vector<int>> &dp){
+    int f(vector<int>& arr,int idx,int &ans,int sum1,int target, vector<vector<int>> &dp){
         if(idx==0){
             int sum2 = target-sum1;
-            return abs(sum1-sum2);
+            return abs(sum2-sum1);
         }
         
         if(dp[idx][sum1]!=-1) return dp[idx][sum1];
         
-        int pick1 = f(arr,idx-1,sum1+arr[idx-1],target,dp);
-        int pick2 = f(arr,idx-1,sum1,target,dp);
+        return dp[idx][sum1] = min(f(arr,idx-1,ans,sum1+arr[idx-1],target,dp),f(arr,idx-1,ans,sum1,target,dp));
         
-        return dp[idx][sum1] = min(pick1,pick2);
     }
     
     int minDifference(vector<int>& arr) {
-        // code here
         int n = arr.size();
-        int sum=0;
+        int ans = INT_MAX;
+        int sum =0;
         
-        for(int i=0;i<arr.size();i++){
+        for(int i=0;i<n;i++){
             sum += arr[i];
         }
         
         vector<vector<int>> dp(n+1,vector<int> (sum+1,-1));
-        return f(arr,n,0,sum,dp);
+        
+        return f(arr,n,ans,0,sum,dp);
+        
     }
 };
