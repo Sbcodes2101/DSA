9661756e4d10de6a4791349e1f2ef6dbb0132cbb
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-08-31 09:58:16 +0530

    Time: 38 ms (34.42%) | Memory: 14.1 MB (9.06%) - LeetSync

---
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int f(vector<int> &scale,int left,int right,vector<vector<int>> &dp){
+        if(left+1>=right) return 0;
+
+        if(dp[left][right]!=-1) return dp[left][right];
+
+        int cost = INT_MAX;
+    
+        for(int i=left+1;i<right;i++){
+            int ans = scale[right]-scale[left];
+            ans += f(scale,left,i,dp);
+            ans += f(scale,i,right,dp);
+            cost = min(cost,ans);
+        }
+
+        return dp[left][right] = cost;
+    }
+
+    int minCost(int n, vector<int>& cuts) {
+        sort(cuts.begin(),cuts.end());
+        vector<int> scale;
+        scale.push_back(0);
+
+        for(int i=0;i<cuts.size();i++){
+            scale.push_back(cuts[i]);
+        }
+
+        scale.push_back(n);
+
+        vector<vector<int>> dp(scale.size(),vector<int> (scale.size(),-1));
+        return f(scale,0,scale.size()-1,dp);
+    }
+};
