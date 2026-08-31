6ecd382d5969d278c7c1d47c3f9cae853da24cd8
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-08-31 13:57:11 +0530

    Time: 388 ms (51.38%) | Memory: 224.1 MB (17.47%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
-    int maximumProfit(vector<int>& prices,int index,bool buy,int no_of_transaction,vector<vector<vector<int>>> &dp){
-        if(index==prices.size() || no_of_transaction == 0){
-            return 0;
-        }
+    int f(vector<int> &prices,int idx,int no_of_transaction,bool buy,vector<vector<vector<int>>> &dp){
+        if(idx==prices.size()) return 0;
+
+        if(no_of_transaction == 0) return 0;
+
+        if(dp[idx][buy][no_of_transaction] != -1) return dp[idx][buy][no_of_transaction];
+
+        int ans=0;
 
         if(buy){
-            return dp[index][buy][no_of_transaction] = max(maximumProfit(prices,index+1,true,no_of_transaction,dp),-prices[index]+maximumProfit(prices,index+1,false,no_of_transaction,dp));
+            ans += max(-prices[idx]+f(prices,idx+1,no_of_transaction,false,dp),f(prices,idx+1,no_of_transaction,true,dp));
         }
 
         else{
-            return dp[index][buy][no_of_transaction] = max(maximumProfit(prices,index+1,false,no_of_transaction,dp),prices[index]+maximumProfit(prices,index+1,true,no_of_transaction-1,dp));
+            ans += max(prices[idx]+f(prices,idx+1,no_of_transaction-1,true,dp),f(prices,idx+1,no_of_transaction,false,dp));
         }
 
-        return 0;
+        return dp[idx][buy][no_of_transaction] = ans;
     }
+
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (3,vector<int> (2,0)));
-        // return maximumProfit(prices,0,true,2,dp);
-
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<=2;j++){
-                for(int k=0;k<=1;k++){
-                    if(k==1){
-                        dp[i][j][k] = max(dp[i+1][j][1],-prices[i]+dp[i+1][j][0]);
-                    }
-                    else{
-                        if(j>0) dp[i][j][k] = max(dp[i+1][j][0],prices[i]+dp[i+1][j-1][1]);
-                    }
-                }
-            }
-        }
-
-        return dp[0][2][1];
+        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int>(3,-1)));
+        return f(prices,0,2,true,dp);
     }
 };
