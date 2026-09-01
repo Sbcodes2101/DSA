f1387ebb421ac6f33d16a5d80fabb57e8dece728
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-09-01 17:05:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (16.84%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    int f(vector<int> &stones,int sum1,int target,int idx,vector<vector<int>> &dp){
-        if(idx==0){
-            int sum2=target-sum1;
-            return abs(sum1-sum2);
+    int f(vector<int>& stones,int idx,int sum,int target,vector<vector<int>> &dp){
+        if(idx==stones.size()){
+            int sum2 = target-sum;
+            return dp[idx][sum] = abs(sum2-sum);
         }
 
-        if(dp[idx][sum1]!=-1) return dp[idx][sum1];
+        if(dp[idx][sum]!=-1) return dp[idx][sum];
 
-        int pick = f(stones,sum1+stones[idx-1],target,idx-1,dp);
-        int not_pick = f(stones,sum1,target,idx-1,dp);
-
-        return dp[idx][sum1] = min(pick,not_pick);
+        return dp[idx][sum] = min(f(stones,idx+1,sum+stones[idx],target,dp),f(stones,idx+1,sum,target,dp));
     }
-
     int lastStoneWeightII(vector<int>& stones) {
         int n = stones.size();
-        int sum = 0;
+        int target = 0;
 
         for(int i=0;i<n;i++){
-            sum += stones[i];
+            target += stones[i];
         }
 
-        vector<vector<int>> dp(n+1,vector<int> (sum+1,-1));
-        return f(stones,0,sum,n,dp);
+        vector<vector<int>> dp(n+1,vector<int> (target+1,-1));
+        return f(stones,0,0,target,dp);
     }
 };
