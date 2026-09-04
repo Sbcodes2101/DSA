c73c3c8e88484cff6e28f656fcfc93a9d03219de
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-09-04 13:59:15 +0530

    Time: 19 ms (96.02%) | Memory: 17.4 MB (91.21%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -5,27 +5,33 @@ public:
         vector<int> LIS(n,1);
         vector<int> count(n,1);
 
-        int maxLen = 1;
+        int maxLen =1;
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
                 if(nums[i]>nums[j]){
+
                     if(LIS[i]<LIS[j]+1){
                         LIS[i]=LIS[j]+1;
-                        count[i]=count[j];
+                        count[i] = count[j];
                     }
-                    else if(LIS[i]==LIS[j]+1){
+
+                    else if(LIS[i]==1+LIS[j]){
                         count[i] += count[j];
                     }
                 }
             }
-            maxLen = max(maxLen, LIS[i]);
+
+            if(maxLen<LIS[i]){
+                maxLen = LIS[i];
+            }
         }
 
-        int result = 0;
+        int ans = 0;
         for(int i=0;i<n;i++){
-            if(LIS[i] == maxLen) result += count[i];
+            if(LIS[i]==maxLen){
+                ans += count[i];
+            }
         }
-
-        return result;
+        return ans;
     }
 };
