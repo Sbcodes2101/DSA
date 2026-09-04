166eef9e398e821dba06920e54f5562fceeb23e7
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-09-04 09:53:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Print Longest Increasing Subsequence/print-longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Print Longest Increasing Subsequence/print-longest-increasing-subsequence.cpp
@@ -1,41 +1,35 @@
 class Solution {
-public:
+  public:
     vector<int> getLIS(vector<int>& arr) {
         int n = arr.size();
-
-        vector<int> lis(n, 1);
-        vector<int> parent(n, -1);
-
-        // Calculate LIS ending at every index
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-
-                if(arr[i] > arr[j] && lis[i] < lis[j] + 1) {
-                    lis[i] = lis[j] + 1;
+        vector<int> LIS(n,1);
+        vector<int> parent(n,-1);
+        
+        vector<int> ans1;
+        int ans = 1;
+        int idx = 0;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i]>arr[j] && LIS[i]<LIS[j]+1) {
+                    LIS[i] = 1+LIS[j];
                     parent[i] = j;
                 }
             }
-        }
-
-        // Find index where LIS is maximum
-        int idx = 0;
-
-        for(int i = 1; i < n; i++) {
-            if(lis[i] > lis[idx]) {
+            
+            if(ans<LIS[i]){
+                ans = LIS[i];
                 idx = i;
             }
         }
-
-        // Reconstruct LIS
-        vector<int> ans;
-
-        while(idx != -1) {
-            ans.push_back(arr[idx]);
+        
+        
+        while(idx != -1){
+            ans1.push_back(arr[idx]);
             idx = parent[idx];
         }
-
-        reverse(ans.begin(), ans.end());
-
-        return ans;
+        
+        reverse(ans1.begin(),ans1.end());
+        
+        return ans1;
     }
 };
