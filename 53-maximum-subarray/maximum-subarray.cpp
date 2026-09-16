25a7da9b905166f7a163f0c2dbdcb492722eb1c9
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-09-16 10:22:03 +0530

    Time: 4 ms (10.62%) | Memory: 71.7 MB (52.26%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,14 +4,12 @@ public:
         int sum = 0;
         int maxi = INT_MIN;
         if(nums.size()==1) return nums[0];
+
         for(int i=0;i<nums.size();i++){
             sum += nums[i];
-            if(sum>0){
-                maxi = max(maxi,sum);
-            }
-            else{
-                maxi = max(maxi,sum);
-                sum=0;
+            maxi = max(maxi,sum);
+            if(sum<0){
+                sum = 0;
             }
         }
         return (maxi==INT_MIN)? 0:maxi;
