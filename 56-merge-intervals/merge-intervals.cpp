ccd9cb01fed24e1abaaeb2e31f59e0be6cd755be
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-09-22 16:22:47 +0530

    Time: 3 ms (89.34%) | Memory: 23.9 MB (60.45%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        // int n = intervals.size();
-        // sort(intervals.begin(),intervals.end());
-        // vector<vector<int>> ans;
-        // for(int i=0;i<n;i++){
-        //     int start = intervals[i][0];
-        //     int end = intervals[i][1];
-        //     if(!ans.empty() && end <= ans.back()[1]) continue;
-        //     for(int j=i+1;j<n;j++){
-        //         if(intervals[j][0] <= end){
-        //             end = max(end,intervals[j][1]);
-        //         }
-        //         else {
-        //             break;
-        //         }
-        //     }
-        //     ans.push_back({start,end});
-
-        int n = intervals.size();
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
+        int cs = intervals[0][0];
+        int ce = intervals[0][1];
+        int n = intervals.size();
 
-        for(int i=0;i<n;i++){
-            if(ans.empty() || intervals[i][0] > ans.back()[1]){
-                ans.push_back(intervals[i]);
+        for(int i=1;i<n;i++){
+            if(ce<intervals[i][0]){
+                ans.push_back({cs,ce});
+                cs = intervals[i][0];
+                ce = intervals[i][1];
             }
 
             else{
-                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+                ce = max(ce,intervals[i][1]);
             }
         }
-    return ans;
+
+        ans.push_back({cs,ce});
+
+        return ans;
     }
 };
