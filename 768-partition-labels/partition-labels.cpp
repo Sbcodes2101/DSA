4f036726107319e63c9b5dbef8d85e8718659ba6
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-09-23 14:07:44 +0530

    Time: 3 ms (24.51%) | Memory: 8.9 MB (65.93%) - LeetSync

---
+++ 768-partition-labels/partition-labels.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.size();
+        vector<int> ans;
+        unordered_map<char,int> mp;
+
+        for(int i=0;i<s.size();i++){
+            mp[s[i]] = i;
+        }
+        
+        int last_index=mp[s[0]];
+        int start = 0;
+
+        for(int i=0;i<n;i++){
+            last_index = max(last_index,mp[s[i]]);
+
+            if(i==last_index){
+                ans.push_back(i-start+1);
+                last_index = mp[s[i+1]];
+                start = i+1;
+            }
+        }
+        
+        return ans;
+    }
+};
