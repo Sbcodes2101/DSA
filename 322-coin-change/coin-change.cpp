851edda1f3acc809d0649f951408d770df5dc35f
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-09-24 10:52:41 +0530

    Time: 55 ms (20.00%) | Memory: 54 MB (7.57%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,56 +1,18 @@
 class Solution {
 public:
     int f(vector<int> &coins,int amount,int idx,vector<vector<int>> &dp){
-        if(amount==0){
-            return 0;
-        }
+        if(amount==0) return 0;
 
         if(idx==0 || amount<0) return 1e9;
 
         if(dp[idx][amount]!=-1) return dp[idx][amount];
 
-        return dp[idx][amount] = min(f(coins,amount,idx-1,dp),1+f(coins,amount-coins[idx-1],idx,dp));
+        return dp[idx][amount] = min(1+f(coins,amount-coins[idx-1],idx,dp),f(coins,amount,idx-1,dp));
     }
-
     int coinChange(vector<int>& coins, int amount) {
-        // int n = coins.size();
-        // vector<vector<int>> dp(n+1,vector<int> (amount+1,0));
-        // // int ans = f(coins,amount,n,dp);
-        // // return (ans==1e9)? -1:ans;
-
-        // for(int i=0;i<=n;i++){
-        //     dp[i][0] = 0;
-        // }
-
-        // for(int i=0;i<=amount;i++){
-        //     dp[0][i] = 1e9;
-        // }
-
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=amount;j++){
-        //         int pick=1e9;
-        //         if(j-coins[i-1]>=0) pick = 1+dp[i][j-coins[i-1]];
-        //         int not_pick = dp[i-1][j];
-        //         dp[i][j] = min(pick,not_pick);
-        //     }
-        // }
-
-        // return (dp[n][amount]==1e9)? -1:dp[n][amount];
-
         int n = coins.size();
-        vector<int> curr(amount+1,1e9);
-
-        curr[0] = 0;
-
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=amount;j++){
-                int pick = 1e9;
-                if(j-coins[i-1]>=0) pick = 1+curr[j-coins[i-1]];
-                int not_pick = curr[j];
-                curr[j] = min(pick,not_pick);
-            }
-        }
-
-        return (curr[amount]==1e9)? -1:curr[amount];
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        int ans = f(coins,amount,n,dp);
+        return (ans==1e9)? -1:ans; 
     }
 };
