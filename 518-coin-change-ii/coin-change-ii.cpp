de03f2094e3ac5f28fca19cab7c422c923aa2799
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-09-25 10:00:30 +0530

    Time: 29 ms (55.22%) | Memory: 37.1 MB (24.47%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
-    int f(int amount,vector<int> &coins,int idx,vector<vector<int>> &dp){
-        if(amount == 0){
-            return 1;
-        }
+    int f(int amount,vector<int>& coins,int idx,vector<vector<int>> &dp){
+        if(amount == 0) return 1;
 
-        if(amount<0 || idx==0) return 0; 
+        if(idx<0 || amount<0) return 0;
 
         if(dp[idx][amount]!=-1) return dp[idx][amount];
-
-        int pick = 0;
-        if(amount-coins[idx-1]>=0) pick = f(amount-coins[idx-1],coins,idx,dp);
+    
         int not_pick = f(amount,coins,idx-1,dp);
+        int pick = 0;
+        if(idx-1>=0) pick = f(amount-coins[idx-1],coins,idx,dp);
 
         return dp[idx][amount] = pick+not_pick;
-    } 
-
+    }
 
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        // vector<vector<double>> dp(n+1,vector<double> (amount+1,0));
-        // return f(amount,coins,n,dp);
-        
-        // for(int i=0;i<=n;i++){
-        //     dp[i][0] = 1;
-        // }
-
-        vector<double> curr(amount+1,0);
-        curr[0] = 1;
-
-        for(double i=1;i<=n;i++){
-            // vector<double> curr(amount+1,0);
-            // curr[0]=1;
-            for(double j=0;j<=amount;j++){
-                if(j-coins[i-1]>=0) curr[j] = curr[j]+curr[j-coins[i-1]];
-            }
-        }
-
-        return curr[amount];
+        int n = coins.size();
+        vector<vector<int>> dp(n+1,vector<int> (amount+1,-1));
+        return f(amount,coins,n,dp);
     }
 };
